72d4b735143a50c968146ec3ff802d53d623892f
aluok <humbert.timede@gmail.com>
2016-08-13 20:44:34 +0200

    First implementation of Configuration

---
+++ src/Garbanzo/Kernel/Configuration.php
@@ -0,0 +1,86 @@
+<?php
+namespace Garbanzo\Kernel;
+
+use Exception;
+use StdClass;
+
+class Configuration {
+
+    const CONFIG_DIRECTORY = __DIR__ . '/../../../config/';
+
+    private $config_path;
+    private $properties;
+    private $environment;
+
+    public function __construct($environment) {
+        $this->config_path = NULL;
+        $this->environment = $environment;
+    }
+
+    public function setConfigRootDirectory($path) {
+        $this->config_path = $path;
+    }
+
+    public function loadFile($fileName) {
+        $path = $this->generateFilePath($fileName);
+        if (! file_exists($path)) {
+            $path = $this->generateFilePath($fileName, true);
+            if (! file_exists($path)) {
+                throw new Exception('File not found :' . $path);
+            }
+        }
+        $this->properties = $this->loadData($path);
+        if ($this->environment !== App::PROD) {
+            $this->addDataFromProd($fileName);
+        }
+    }
+
+    protected function loadData($path) {
+        return json_decode(file_get_contents($path));
+    }
+
+    protected function addDataFromProd($fileName) {
+        $path = $this->generateFilePath($fileName, true);
+        if (! file_exists($path)) {
+            throw new Exception('File not found: config file ' . $path . '.json for production.');
+        }
+        $newProperties = $this->loadData($path);
+        $this->properties = $this->getDataToConcat($this->properties, $newProperties);
+        //$this->concatData($dataToAdd);
+    }
+
+    protected function getDataToConcat($oldProperties, $newProperties) {
+        $property = $oldProperties;
+        foreach (get_object_vars($newProperties) as $name => $newProperty) {
+            if (! property_exists($oldProperties, $name)) {
+                $property->$name = $newProperty;
+            } elseif (is_object($newProperty)) {
+                $property->$name = $this->getDataToConcat($oldProperties->$name, $newProperty);
+            }
+        }
+        return $property;
+    }
+
+    protected function generateFilePath($fileName, $default = false) {
+        $file = ($this->config_path !== NULL) ? $this->config_path : self::CONFIG_DIRECTORY;
+        $file.= $fileName;
+        if ( (! $default) && $this->environment !== App::PROD) {
+            $file.= '_' . $this->environment;
+        }
+        $file.= '.json';
+        return $file;
+    }
+
+    public function getProperties() {
+        return $this->properties;
+    }
+
+    public function get($propertyName) {
+        $propertyNames = explode('.', $propertyName);
+        $property = $this->properties;
+        foreach ($propertyNames as $namePart) {
+            $property = $property->$namePart;
+        }
+        return $property;
+    }
+}
---
+++ tests/Garbanzo/Kernel/ConfigurationTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace Test\Garbanzo\Kernel;
+
+use PHPUnit\Framework\TestCase;
+use Garbanzo\Kernel\Configuration;
+use Garbanzo\Kernel\App;
+use StdClass;
+
+class ConfigurationTest extends TestCase {
+
+    /**
+     * @test
+     */
+    public function itCanLoadADefaultProductionConfigFile() {
+        $configuration = new Configuration(App::PROD);
+        $configuration->loadFile('config');
+        $this->assertNotNull($configuration->get('secret'));
+    }
+
+    /**
+     * @test
+     * @depends itCanLoadADefaultProductionConfigFile
+     */
+    public function itCanLoadACustomProductionConfigFile() {
+        $configuration = new Configuration(App::PROD);
+        $configuration->setConfigRootDirectory(__DIR__ . '/../../config/');
+        $configuration->loadFile('test');
+        $expected = new StdClass();
+        $expected->prop1 = 'VALUE1';
+        $expected->prop4 = 'VALUE4';
+        $this->assertEquals($expected, $configuration->get('test1'));
+        $this->assertEquals("Val3", $configuration->get('test3'));
+        $this->assertEquals('prod', $configuration->get('tempered'));
+    }
+
+    /**
+     * @test
+     * @depends itCanLoadADefaultProductionConfigFile
+     */
+    public function itCanGetAMultiPartProperty() {
+        $configuration = new Configuration(App::PROD);
+        $configuration->setConfigRootDirectory(__DIR__ . '/../../config/');
+        $configuration->loadFile('test');
+        $this->assertEquals('VALUE1', $configuration->get('test1.prop1'));
+    }
+
+    /**
+     * @test
+     * @depends itCanLoadACustomProductionConfigFile
+     */
+    public function itCanLoadADevelopmentConfigFile() {
+        $configuration = new Configuration(App::DEV);
+        $configuration->setConfigRootDirectory(__DIR__ . '/../../config/');
+        $configuration->loadFile('test');
+        $this->assertEquals('VALUE1', $configuration->get('test1.prop1'));
+        $this->assertEquals('VALUE2', $configuration->get('test1.prop2'));
+        $this->assertEquals('Val', $configuration->get('test2'));
+    }
+
+    /**
+     * @test
+     * @depends itCanLoadACustomProductionConfigFile
+     */
+    public function itCanConcatADevAndAProductionConfigFile() {
+        $configuration = new Configuration(App::DEV);
+        $configuration->setConfigRootDirectory(__DIR__ . '/../../config/');
+        $configuration->loadFile('test');
+        $this->assertEquals('VALUE1', $configuration->get('test1.prop1'));
+        $this->assertEquals('VALUE2', $configuration->get('test1.prop2'));
+        $this->assertEquals('VALUE4', $configuration->get('test1.prop4'));
+        $this->assertEquals('Val', $configuration->get('test2'));
+        $this->assertEquals('Val3', $configuration->get('test3'));
+        $this->assertEquals('dev', $configuration->get('tempered'));
+    }
+
+}
